5c67d68686b8f5f20df9760370bedbb25f54955c
Alejandroprz98 <alejandroperezrosas@hotmail.com>
2024-05-09 16:19:25 -0600

    modificaciones

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@
 import './App.css';
 import addidas from './assets/img/ad negro.jpg';
 import tenis from './assets/img/tenis.avif';
-
+import tenis2 from './assets/img/tenis2.avif';
+import tenis3 from './assets/img/tenis3.avif';
+import tenis4 from './assets/img/tenis4.avif';
 function App() {
   return (
   
     <div class="body"> 
     <header>
+    <a href="https://www.adidas.mx/">
     < img class= "logo" src={addidas} />  
+    </a>
 </header>
 <div class="pagina">
     <div class="producto-img">
-        <div class="imagen"><img src={tenis} alt="" height=""width=""/></div>
+        <div class="imagen"><img src={tenis} alt=""height="500px"  width="500px"/></div>
+        <div class="imagen"><img src={tenis2} alt="" height="500px"  width="500px"/></div>
+        <div class="imagen"><img src={tenis3} alt="" height="500px"  width="500px"/></div>
+        <div class="imagen"><img src={tenis4} alt="" height="500px"  width="500px"/></div>
     </div>
     <div class="producto">
         <h2 class="producto-nombre">
@@ -34,6 +41,18 @@ function App() {
         </div>
     </div>
 </div>
+<footer>
+        <nav class="lista">
+            <ul class="lista-contenido">
+                <li class="contenido">Configuración de las cookies</li>
+                <li class="contenido">Nuestros Datos</li>
+                <li class="contenido">Términos y Condiciones</li>
+                <li class="contenido">Uso del Sitio</li>
+                <li class="contenido">Términos y Condiciones Promociones</li>
+                <li class="contenido">Privacidad de Videovigilancia</li>
+            </ul>
+        </nav>
+    </footer>
     </div > 
     
 
